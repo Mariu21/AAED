9c402ee40eef85cd5fa119457b2b3e10a10a3d40
Mariu21 <a25562@@svalero.com>
2021-08-09 08:34:56 +0200

    version 1.1 Elimino el código para crear features según AA

--- src/main/java/com/sanvalero/aaed/Application.java
+++ src/main/java/com/sanvalero/aaed/Application.java
@@ -1,11 +1,9 @@
 package com.sanvalero.aaed;
 
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        App app = new App();
-        app.menuPrincipal();
+
     }
-
 
 }
