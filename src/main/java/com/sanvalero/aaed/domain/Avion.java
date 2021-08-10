3ab5d44475ad326f01c250d080f672d444e00e4f
Mariu21 <a25562@@svalero.com>
2021-08-10 13:11:45 +0200

    Nuevas clases

--- src/main/java/com/sanvalero/aaed/domain/Avion.java
+++ src/main/java/com/sanvalero/aaed/domain/Avion.java
@@ -1,8 +1,119 @@
 package com.sanvalero.aaed.domain;
 
-
+import java.util.Objects;
 
 public class Avion {
+    String modelo;
+    String numSerie;
+    String compania;
+    int asiento;
+    float combustible;
+    float pesoavion;
+
+    Vuelo vuelo;
 
 
+    public Avion(String modelo, String numSerie, String compania, int asiento,
+                 float combustible, float pesoavion) {
+        this.modelo = modelo;
+        this.numSerie = numSerie;
+        this.compania = compania;
+        this.asiento = asiento;
+        this.combustible = combustible;
+        this.pesoavion = pesoavion;
+        this.vuelo = vuelo;
+    }
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+
+    }
+
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+
+    }
+
+
+    public String getCompania() {
+        return compania;
+    }
+
+    public void setCompania(String compania) {
+        this.compania = compania;
+
+    }
+
+
+    public int getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(int asiento) {
+        this.asiento = asiento;
+    }
+
+
+    public float getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(float combustible) {
+        this.combustible = combustible;
+
+    }
+
+    public float getPesoavion() {
+        return pesoavion;
+    }
+
+    public void setPesoavion(float pesoavion) {
+        this.pesoavion = pesoavion;
+
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public void setVuelo(Vuelo vuelo) {
+        this.vuelo = vuelo;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.valueOf("Avion{" +
+                "Modelo='" + modelo + '\'' +
+                "Número de serie='" + numSerie + '\'' +
+                ", Compañia='" + compania + '\'' +
+                ", Nº asientos='" + asiento + '\'' +
+                ", Combustible='" + combustible + " litros" + '\'' +
+                ", peso='" + pesoavion + " kg" + '\'' +
+                '}');
+    }
+
+    @Override
+    public int hashCode() {
+        return numSerie.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Avion)) return false;
+        Avion avion = (Avion) o;
+        return Objects.equals(getNumSerie(), avion.getNumSerie());
+    }
 }
+
+
--- src/main/java/com/sanvalero/aaed/domain/Vuelo.java
+++ src/main/java/com/sanvalero/aaed/domain/Vuelo.java
@@ -1,6 +1,129 @@
 package com.sanvalero.aaed.domain;
 
+import java.util.Objects;
+
 public class Vuelo {
 
+    String nvuelo;
+    String origen;
+    String destino;
+    String fecha;
+    String horario;
+    float km;
+    int escala ;
+
+
+    public Vuelo(String nvuelo, String origen,String destino, String fecha,
+                 String horario, float km, int escala) {
+
+        this.nvuelo=nvuelo;
+        this.origen=origen;
+        this.destino=destino;
+        this.fecha=fecha;
+        this.horario=horario;
+        this.km=km;
+        this.escala=escala;
+    }
+
+
+
+    public String getNvuelo() {
+        return nvuelo;
+    }
+
+    public void setNvuelo(String nvuelo) {
+        this.nvuelo = nvuelo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public float getKm() {
+        return km;
+    }
+
+    public void setKm(float km) {
+        this.km = km;
+    }
+
+    public int getEscala() {
+        return escala;
+    }
+
+    public void setEscala(int escala) {
+        this.escala = escala;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "Vuelo{" +
+                "Nº Vuelo='" + nvuelo + '\'' +
+                ", Origen='" + origen + '\'' +
+                ", Destino='" + destino + '\'' +
+                ", Fecha del vuelo='" + fecha + '\'' +
+                ", Hora de salida='" + horario + '\'' +
+                ", Distancia'" + km + " km" + '\'' +
+                ", Nº Escalas='" + escala + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+
+        final Vuelo other = (Vuelo) obj;
+        if (!Objects.equals(this.nvuelo, other.nvuelo)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    @Override
+    public int hashCode() {
+
+        return nvuelo.hashCode();
+    }
 
 }
