253fee9d493a0fd95bf5dd821e17c01ecf52da2d
Mariu21 <a25562@@svalero.com>
2021-08-11 12:51:41 +0200

    modificacion test

--- src/test/java/com/sanvalero/aaed/PasajeroTest.java
+++ src/test/java/com/sanvalero/aaed/PasajeroTest.java
@@ -8,23 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PasajeroTest {
 
     @Test
-
     void registrarPsajero() {
         String nombre = "Pepe";
         String apellido = "Sánchez Sánchez";
         int edad = 35;
         String dni = "25215468G";
         String email = "gidjs@gmail";
-        int equipaje= 2;
-        float pesoequipaje= 20F;
+        int equipaje = 2;
+        float pesoequipaje = 20F;
 
         Pasajero testPasajero = new Pasajero(nombre, apellido, edad, dni, email, equipaje, pesoequipaje);
 
 
         assertEquals(testPasajero.getNombre(), nombre);
         assertEquals(testPasajero.getApellido(), apellido);
-        assertEquals(testPasajero.getEdad(), edad);
         assertEquals(testPasajero.getDni(), dni);
+        assertEquals(testPasajero.getEdad(), edad);
         assertEquals(testPasajero.getEmail(), email);
         assertEquals(testPasajero.getEquipaje(), equipaje);
         assertEquals(testPasajero.getPesoequipaje(), pesoequipaje);
@@ -32,8 +31,6 @@ public class PasajeroTest {
 
 
     @Test
-
-
     void testEqualsPasajero() {
 
         String nombre = "Pepe";
@@ -41,21 +38,18 @@ public class PasajeroTest {
         int edad = 35;
         String dni = "25215468G";
         String email = "gidjs@gmail";
-        int equipaje= 2;
-        float pesoequipaje= 20F;
+        int equipaje = 2;
+        float pesoequipaje = 20F;
 
 
+        Pasajero testPasajero = new Pasajero(nombre, apellido, edad, dni, email, equipaje, pesoequipaje);
 
-
-        Pasajero testPasajero = new Pasajero(nombre, apellido, edad,dni, email, equipaje, pesoequipaje);
-
-        Pasajero otroTestPasajero = new Pasajero(nombre, apellido, edad,dni, email, equipaje, pesoequipaje);
+        Pasajero otroTestPasajero = new Pasajero(nombre, apellido, edad, dni, email, equipaje, pesoequipaje);
 
         assertEquals(testPasajero, otroTestPasajero);
 
 
-
     }
-
-
 }
+
+
